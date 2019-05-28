ab92e57a70e692a1474e378abd36e42191fc1a65
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-28 14:58:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-if phrase !== phrase.upcase
+if phrase != phrase.upcase
   puts "HUH?! SPEAK UP, SONNY!"
 elsif phrase == phrase.upcase
   puts "NO, NOT SINCE 1938!"
