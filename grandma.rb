4bb8a33fc669bd0f6eec052801dcefd377d2c90e
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-28 15:02:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma(phrase)
 if phrase != phrase.upcase
   return "HUH?! SPEAK UP, SONNY!"
 elsif phrase == phrase.upcase && phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+  return "I LOVE YOU TOO PUMPKIN!"
 else
   return "NO, NOT SINCE 1938!"
 end
